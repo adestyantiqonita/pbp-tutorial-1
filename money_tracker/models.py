36094efaf897a2027914f14a7c450aa8b0843a37
adestyantiqonita <94448470+adestyantiqonita@users.noreply.github.com>
2023-02-15 18:48:58 +0700

    Perbaiki F pada CharField

--- money_tracker/models.py
+++ money_tracker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class TransactionRecord(models.Model):
     name = models.CharField(max_length=50)
-    type = models.Charfield(max_length=20)
+    type = models.CharField(max_length=20)
     amount = models.IntegerField()
     date = models.DateTimeField(auto_now_add=True)
     description = models.TextField()
