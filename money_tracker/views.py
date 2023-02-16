820710d3b6460fa8ba69f8450f145ced6fe04a74
adestyantiqonita <94448470+adestyantiqonita@users.noreply.github.com>
2023-02-16 22:05:47 +0700

    transaction update

--- money_tracker/views.py
+++ money_tracker/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from money_tracker.models import TransactionRecord
 
 # Create your views here.
 
 def show_tracker(request):
-    return render(request, "tracker.html")
+    transaction_data = TransactionRecord.objects.all()
+    context = {
+        'list_of_transactions': transaction_data,
+        'name': 'Qonita Adestyanti'
+    }
+    return render(request, "tracker.html", context)
+
+{% for transaction in list_of_transactions %}
+    <tr>
+        <td>{{transaction.name}}</td>
+        <td>{{transaction.type}}</td>
+        <td>{{transaction.amount}}</td>
+        <td>{{transaction.date}}</td>
+        <td>{{transaction.description}}</td>
+    </tr>
+{% endfor %}
