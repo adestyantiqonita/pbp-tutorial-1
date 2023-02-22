f624132ddbb998eca79bb9ab92dbbf58d0e9de0f
adestyantiqonita <94448470+adestyantiqonita@users.noreply.github.com>
2023-02-22 13:36:01 +0700

    membuat form registrasi final

--- money_tracker/views.py
+++ money_tracker/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from money_tracker.models import TransactionRecord
+from django.http import HttpResponse
+from django.core import serializers
+from django.http import HttpResponseRedirect
+from money_tracker.forms import TransactionRecordForm
+from django.urls import reverse
+
 
 # Create your views here.
 
@@ -10,3 +16,18 @@ def show_tracker(request):
         'name': 'Qonita Adestyanti'
     }
     return render(request, "tracker.html", context)
+
+def show_xml():
+    data = TransactionRecord.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def create_transaction(request):
+    form = TransactionRecordForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('money_tracker:show_tracker'))
+
+    context = {'form': form}
+    return render(request, "create_transaction.html", context)
+
