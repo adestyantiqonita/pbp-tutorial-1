7698a934335a191aa6e6eb8554d3c0cc2f473e18
adestyantiqonita <94448470+adestyantiqonita@users.noreply.github.com>
2023-03-08 12:45:41 +0700

    tutorial 4

--- money_tracker/urls.py
+++ money_tracker/urls.py
@@ -7,6 +7,9 @@ from money_tracker.views import create_transaction
 from money_tracker.views import register
 from money_tracker.views import login_user
 from money_tracker.views import logout_user
+from money_tracker.views import modify_transaction
+from money_tracker.views import delete_transaction
+
 
 app_name = 'money_tracker'
 
@@ -16,8 +19,10 @@ urlpatterns = [
          path('json/', show_json, name='show_json'),
          path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
          path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
-         path('create', create_transaction, name='create_transaction'),
+         path('create/', create_transaction, name='create_transaction'),
          path('register/', register, name='register'),
          path('login/', login_user, name='login'),
          path('logout/', logout_user, name='logout'),
+         path('modify/<int:id>', modify_transaction, name='modify_transaction'),
+         path('delete/<int:id>', delete_transaction, name='delete_transaction'),
 ]
